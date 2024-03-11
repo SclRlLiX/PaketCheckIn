ff967b7290f85e09fcda7cdb80a678ce23ae4f83
SclRlLiX <georg.m985@gmail.com>
2024-03-11 13:19:13 +0100

    Added Error Class to display Error Message in MessageBox

---
+++ PaketCheckIn/ApplicationError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaketCheckIn
+{
+    internal class ApplicationError
+    {
+        public ApplicationError(string ErrorMessage)
+        {
+            MessageBox.Show(ErrorMessage, "Fehler",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            System.Windows.Application.Current.Shutdown();
+            return;
+        }
+    }
+}
