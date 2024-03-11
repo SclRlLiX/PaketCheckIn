42c49a766c35cca21ff0f3327596f22bd7a0dad0
SclRlLiX <georg.m985@gmail.com>
2024-03-11 13:20:01 +0100

    Added Class to get all Packages from Archive Folder

---
+++ PaketCheckIn/GetPackagesFromSvnFolder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PaketCheckIn
+{
+    internal class GetPackagesFromSvnFolder
+    {
+        public string[] packageFolders = Directory.GetDirectories(App.SvnArchivePath);
+        public ArrayList packageFoldersWithSvnFolder = new ArrayList();
+        public ArrayList packageFoldersWithoutSvnFolder = new ArrayList();
+
+         ArrayList GetPackagesWithSvnFolder() 
+        {
+            foreach (string svnFolder in packageFolders)
+            {
+                if(Directory.Exists(svnFolder + @"\.svn"))
+                {
+                    packageFoldersWithSvnFolder.Add(svnFolder);
+                }
+            }
+
+            return packageFoldersWithSvnFolder; 
+        }
+    }
+}
